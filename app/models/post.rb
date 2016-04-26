aacd7de250e992949f2c81934d18f598725403d7
bzwik <blake.zwikler@gmail.com>
2016-04-26 00:45:31 -0400

    Index Changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,6 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
 
-	has_attached_file :image, styles: { medium: "500x500>", thumb: "200x80" }, default_url: ""
+	has_attached_file :image, styles: { medium: "300x300", thumb: "100x64#" }, default_url: ""
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
